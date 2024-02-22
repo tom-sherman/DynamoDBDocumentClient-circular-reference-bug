eec6a912c16a8a4c237fafba41d0bb40403df4a3
Tom Sherman <the.tomsherman@gmail.com>
2024-02-22 09:59:08 +0000

    Update reproduction

--- main.js
+++ main.js
@@ -1,24 +1,58 @@
 const { DynamoDB } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
 
+const tryThenLog = async (fn) => {
+  try {
+    await fn();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
 const client = DynamoDBDocumentClient.from(
   new DynamoDB({
     endpoint: "http://localhost:8000",
   })
 );
 
-const a = {
-  toJSON() {
-    return {
-      a: "a",
-    };
-  },
-};
-a.a = a;
+async function main() {
+  const a = {
+    toJSON() {
+      return {
+        a: "a",
+      };
+    },
+  };
+  a.a = a;
 
-client.send(
-  new PutCommand({
-    TableName: "test",
-    Item: a,
-  })
-);
+  console.log("With toJSON:\n");
+
+  try {
+    await client.send(
+      new PutCommand({
+        TableName: "test",
+        Item: a,
+      })
+    );
+  } catch (e) {
+    console.error(e);
+  }
+
+  console.log("\n\nWithout toJSON:\n");
+
+  const b = {};
+  b.b = b;
+
+  try {
+    await client.send(
+      new PutCommand({
+        TableName: "test",
+        Item: b,
+      })
+    );
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main();
